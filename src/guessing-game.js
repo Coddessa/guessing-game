f01fb19d362e919b33d8cf3563dc48f85addda14
Nadzeya Vaskouskaya <nadiavaskovskaya@gmail.com>
2024-10-27 22:54:12 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.x = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.x = Math.round((this.min + this.max) / 2);
+  }
 
-    }
+  guess() {
+    return this.x;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.x;
+    this.x = Math.round((this.min + this.max) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.x;
+    this.x = Math.round((this.min + this.max) / 2);
+  }
 }
 
 module.exports = GuessingGame;
